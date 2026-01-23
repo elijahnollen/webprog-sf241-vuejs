16a955577c2c64efbc6b6c123b7d09208fb0399e
Elijah Crisehea Nollen <emnollen@student.apc.edu.ph>
2026-01-23 15:09:08 +0800

    Create the Supabase client

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,32 +1,21 @@
-<template>
-  <div id="app">
-    <h1>My Personal Profile, Fruits, and Vegetables</h1>
-    
-    <div class="food-container">
-      <food-item/>
-      <food-item2/>
-    </div>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
 
-    <hr> <PersonalProfile/>
-  </div>
-</template>
+const instruments = ref([])
 
-<script>
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+
+onMounted(() => {
+   getInstruments()
+})
 </script>
 
-<style>
-
-@font-face {
-    font-family: 'Shantell Sans'; 
-    src: url('https://cdn.jsdelivr.net/gh/elijahnollen/web-programming-practice@main/home/fonts/ShantellSans%5BBNCE%2CINFM%2CSPAC%2Cital%2Cwght%5D.woff2') format('woff2-variations'),
-         url('https://cdn.jsdelivr.net/gh/elijahnollen/web-programming-practice@main/home/fonts/ShantellSans%5BBNCE%2CINFM%2CSPAC%2Cital%2Cwght%5D.woff2') format('woff2');
-    font-weight: 300 800;
-}
-
-.food-container {
-  display: flex;
-  flex-wrap: wrap;
-}
-
-
-</style>
+<template>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
+</template>

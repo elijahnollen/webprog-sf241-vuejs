ac73098ccc7c50c7c70554adb4f44a6fc1dd6d91
Elijah Crisehea Nollen <emnollen@student.apc.edu.ph>
2026-01-27 15:09:34 +0800

    fixed positions of import

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -6,13 +6,13 @@ import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
-import Commentform from './components/Commentform.vue'
 import Comment from './components/Comment.vue'
+import Commentform from './components/Commentform.vue'
 
 const app = createApp(App)
 app.component('PersonalProfile', PersonalProfile)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
-app.component('comment-form', Commentform)
 app.component('comment', Comment)
+app.component('comment-form', Commentform)
 app.mount('#app')

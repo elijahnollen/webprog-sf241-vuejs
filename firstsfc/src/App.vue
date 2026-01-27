5ac61c88bfe49605688465ad215a79c0dd5512c3
Elijah Crisehea Nollen <emnollen@student.apc.edu.ph>
2026-01-27 15:19:47 +0800

    added personal profile

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,10 +1,32 @@
 <template>
-  <h1>Food</h1>
-  <food-item/>
-  <comment-form/>
-  <comment/>
-   
+  <div id="app">
+    <h1>My Personal Profile, Fruits, and Vegetables</h1>
+    
+    <div class="food-container">
+      <food-item/>
+      <comment-form/>
+      <comment/>
+    </div>
 
+    <hr> <PersonalProfile/>
+  </div>
 </template>
 
-<script></script>
+<script>
+</script>
+
+<style>
+
+@font-face {
+    font-family: 'Shantell Sans'; 
+    src: url('https://cdn.jsdelivr.net/gh/elijahnollen/web-programming-practice@main/home/fonts/ShantellSans%5BBNCE%2CINFM%2CSPAC%2Cital%2Cwght%5D.woff2') format('woff2-variations'),
+         url('https://cdn.jsdelivr.net/gh/elijahnollen/web-programming-practice@main/home/fonts/ShantellSans%5BBNCE%2CINFM%2CSPAC%2Cital%2Cwght%5D.woff2') format('woff2');
+    font-weight: 300 800;
+}
+
+.food-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+
